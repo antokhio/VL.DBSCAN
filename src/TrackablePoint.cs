09ec7dd2ce70bd894651cdaa546eb636ff361d8e
antokhio <antokhio@gmail.com>
2025-02-21 15:41:17 +0300

    removed ClusterT, PointT, made cluster generic, bit cleanup, added help

--- src/TrackablePoint.cs
+++ src/TrackablePoint.cs
@@ -14,21 +14,11 @@ public class TrackablePoint : IPointData
     /// Creates a TrackablePoint from a Vector2 to be used with VL DBSCAN plugin
     /// </summary>
     /// <param name="vector"></param>
-    public TrackablePoint(Vector2 vector) =>
-        _point = new Point(vector.X, vector.Y);
+    public TrackablePoint(Vector2 position) =>
+        _point = Extensions.ToPoint(position);
 
     /// <summary>
     /// DBSCAN library's point
     /// </summary>
     public Point Point => _point;
-
-    /// <summary>
-    /// Converts a TrackablePoint to a Vector2 for further use in VL
-    /// </summary>
-    /// <param name="point"></param>
-    /// <returns></returns>
-    public static Vector2 ToVector2(TrackablePoint point)
-    {
-        return new Vector2((float)point._point.X, (float)point._point.Y);
-    }
 }
